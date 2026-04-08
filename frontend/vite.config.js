d8f69bbbfb4a83b5f2b936c317bf5cb3ce3b743d
tmd2052 <tmd2052@naver.com>
2026-04-08 22:20:00 +0900

    도커 테스트

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 9000,
     proxy: {
       "/auth": {
         target: "http://localhost:8080",
